9ed05418369de3bfcbeb61017d1c6bf5c68c3f1a
abusalehkhan12 <abusaleh.khan12@gmail.com>
2020-05-12 06:07:57 +0530

    Updates

---
+++ precache-manifest.1386a08932add73197d92a947ece5068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ae5440b8fb68bed5c765ee77001351",
+    "url": "/TGS-intern/index.html"
+  },
+  {
+    "revision": "fc384f952d0d0b1de5bd",
+    "url": "/TGS-intern/static/css/main.de39baa2.chunk.css"
+  },
+  {
+    "revision": "3aa062d824e3b83beaa0",
+    "url": "/TGS-intern/static/js/2.c46f2fbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TGS-intern/static/js/2.c46f2fbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc384f952d0d0b1de5bd",
+    "url": "/TGS-intern/static/js/main.c6932812.chunk.js"
+  },
+  {
+    "revision": "8b815f8c6c9d4c65e980",
+    "url": "/TGS-intern/static/js/runtime-main.cb77bf03.js"
+  }
+]);
